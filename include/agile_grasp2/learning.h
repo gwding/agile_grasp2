2b12209137f17596bb3013121107e090e5f2482d
andreas <atp@ccs.neu.edu>
2016-05-26 12:03:13 -0400

    add BSD 2-CLAUSE license

--- include/agile_grasp2/learning.h
+++ include/agile_grasp2/learning.h
@@ -1,7 +1,7 @@
 /*
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2015, Andreas ten Pas
+ *  Copyright (c) 2016, Andreas ten Pas
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
